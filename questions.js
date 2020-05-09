1c569f53c06e0cd7727e8c44877cc4b45ad230e9
Jaylan <jaylanwienckowski@gmail.com>
2020-05-09 17:42:08 -0500

    initial commit

---
+++ questions.js
@@ -0,0 +1,20 @@
+// Quiz Questions
+const questions = [
+{
+    title: "Who was not a president?",
+    choices: ["Geroge W. Bush", "Abraham Lincoln", "Jimmy Hendrix"],
+    answer: "Jimmy Hendrix"
+},{
+    title: "What killed Abraham Lincoln?",
+    choices: ["A bullet", "A spiked drink", "Car crash"],
+    answer: "A bullet"
+},{
+    title: "Who was George Bush's father?",
+    choices: ["Frank", "George", "Kyle"],
+    answer: "George"
+},{
+    title: "What American bill is George Washington's face on? ",
+    choices: ["$20", "$1", "$50"],
+    answer: "$1"
+}];
+
